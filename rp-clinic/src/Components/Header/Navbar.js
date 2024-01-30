22f8639bac2419519b8b96792b3b2191fe384928
saan-saurabh <saurabhanshraj@gmail.com>
2024-01-31 01:43:21 +0530

    Completed Navbar Ui for desktop, created a crousel  ux, added  tittle logo,

--- rp-clinic/src/Components/Header/Navbar.js
+++ rp-clinic/src/Components/Header/Navbar.js
@@ -5,12 +5,18 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='navbar-main-div'>
-        <div>
-            <Logo/>
+      <div>
+        <Logo />
+      </div>
+      <div className='navbar-make-appointment-button'>
+        <div className='navbar-right-pane'>
+          <button className='list-menu'>HOME</button>
+          <button className='list-menu'>SERVICES</button>
+          <button className='list-menu'>CONTACT</button>
+          <button className='list-menu'>ABOUT</button>
+          <button className='list-button'>MAKE APPOINTMENT</button>
         </div>
-        <div>
-            Other stuff
-        </div>
+      </div>
     </div>
   );
 }
