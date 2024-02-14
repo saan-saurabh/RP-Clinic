1c59326be57b3f24df539e41fb489f4f55335af5
saan-saurabh <saurabhanshraj@gmail.com>
2024-02-14 23:48:18 +0530

    css changes for doctor section and background img addition

--- rp-clinic/src/Components/Testimonials/TestimonialPanel.js
+++ rp-clinic/src/Components/Testimonials/TestimonialPanel.js
@@ -4,10 +4,10 @@ import './TestimonialPanel.css';
 
 function TestimonialPanel() {
   const userFeedbacks = [
-    {username: "Saurabh K", feedbackDate:"12/12/2023", ratingValue:"4", feedback:'Dr. Sanjay is a well experienced doctor'},
-    {username: "Abhi", feedbackDate:"01/01/2024", ratingValue:"5", feedback:'Very good service'},
+    {username: "Saurabh K", feedbackDate:"12/12/2023", ratingValue:"5", feedback:'Dr. Sanjay is a well experienced doctor'},
+    {username: "Abhi", feedbackDate:"01/01/2024", ratingValue:"4", feedback:'Very good service'},
     {username: "Suresh", feedbackDate:"10/01/2024", ratingValue:"4", feedback:'Service is good and Doctor is very cooperative'},
-    {username: "AK", feedbackDate:"28/12/2023", ratingValue:"4.5", feedback:'Please visit this clinic if you have any orthopedic problem'}
+    {username: "AK", feedbackDate:"28/12/2023", ratingValue:"3", feedback:'Please visit this clinic if you have any orthopedic problem'}
     ]
   return (
     <div className='testimonial-panel-main-div'>
