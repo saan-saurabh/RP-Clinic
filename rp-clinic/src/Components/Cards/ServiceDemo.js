2759ce6daa4038b121a96baee0597bc0e22290a6
saan-saurabh <saurabhanshraj@gmail.com>
2024-02-24 23:45:13 +0530

    Service section dummy component modification

--- rp-clinic/src/Components/Cards/ServiceDemo.js
+++ rp-clinic/src/Components/Cards/ServiceDemo.js
@@ -4,6 +4,10 @@ import './ServiceDemo.css';
 function ServiceDemo() {
     return (
         <div className='service-section-main-div'>
+            <div className='service-section-header'>
+                Our Services
+            </div>
+        <div className='service-section-description-div'>
             <div className='service-section-div-one'>
 
             </div>
@@ -25,23 +29,42 @@ function ServiceDemo() {
                     </div>
                 </div>
                 <div className='service-card-one'>
+                    <div className='description-wrapper'>
+                    <div className='number-header'>
+                        2
+                    </div>
+                    <div className='description-block'>
                     <div className='service-header'>
                     Non-Surgical Treatments
                     </div>
                     <div className='service-description'>
                     <p> We Provide non-surgical treatments which include physical therapy,rehabilitative eercises,splinting,medications.</p>
                     </div>
+                    </div>
+                    </div>
                 </div>
                 <div className='service-card-one'>
+                    <div className='description-wrapper'>
+                    <div className='number-header'>
+                        3
+                    </div>
+                    <div className='description-block'>
                     <div className='service-header'>
                     Surgical Procedures
                     </div>
                     <div className='service-description'>
                     <p>RP Clinic provides all kind of orthopedic srugries including hip or knee replacement,arthroscopy,fracture repair,ligament
-                     or tendon repair and spinal surgery.</p>
+                        or tendon repair and spinal surgery.</p>
+                    </div>
+                    </div>
                     </div>
                 </div>
                 <div className='service-card-one'>
+                    <div className='description-wrapper'>
+                    <div className='number-header'>
+                        4
+                    </div>
+                    <div className='description-block'>
                     <div className='service-header'>
                     Diagnostic Imaging
                     </div>
@@ -49,8 +72,15 @@ function ServiceDemo() {
                     <p>RP Clinic has advanced imaging technology, such as -rays,MRI,CT scans and ultrasound machines to help diagnose and evaluate
                         orthopedic conditions.</p>
                     </div>
+                    </div>
+                    </div>
                 </div>
                 <div className='service-card-one'>
+                    <div className='description-wrapper'>
+                    <div className='number-header'>
+                        5
+                    </div>
+                    <div className='description-block'>
                     <div className='service-header'>
                     Sports Medicine
                     </div>
@@ -58,8 +88,15 @@ function ServiceDemo() {
                     <p>We provide services like injury evaluations,concussion management,performance optimization and personalized treatment
                         plan for sports-related orthopedic conditions.</p>
                     </div>
+                    </div>
+                    </div>
                 </div>
                 <div className='service-card-one'>
+                    <div className='description-wrapper'>
+                    <div className='number-header'>
+                        6
+                    </div>
+                    <div className='description-block'>
                     <div className='service-header'>
                     Rehabilitation & Physical Therapy
                     </div>
@@ -67,10 +104,13 @@ function ServiceDemo() {
                     <p> We also have dedicated rehabiliation centres or work closely with physical therapy provides to help patients
                         recover and regain function after surgery or injury.</p>
                     </div>
+                    </div>
+                    </div>
                 </div>
 
             </div>
 
+        </div>
         </div>
     )
 }
