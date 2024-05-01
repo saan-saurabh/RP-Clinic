72687838629e728f7c4f9551ecc4e232830ce44a
saan-saurabh <saurabhanshraj@gmail.com>
2024-05-01 18:41:09 +0530

    Added media queries for Service, About and Userfeedback component

--- rp-clinic/src/Routes/About.js
+++ rp-clinic/src/Routes/About.js
@@ -7,24 +7,24 @@ function About() {
     <div className='about-main-div'>
       <div className='about-us-description'>
         <h1 className='letter-heading'>A Letter from doctor's desk..</h1>
-      Dear Valued Patients,
-      <br/>
-      <br/>
-Welcome to RP Clinic, where we prioritize your orthopedic health and well-being above all else. As a leading establishment in the field of orthopedics within the health domain, our mission is crystal clear: to provide unparalleled quality health services that cater to your unique needs and exceed your expectations.
-<br/>
-<br/>
-At RP Clinic, we envision a future where every individual can enjoy a life free from the limitations of orthopedic conditions. Our commitment to excellence drives us to continuously innovate, educate, and deliver compassionate care to each and every patient who walks through our doors.
-<br/>
-<br/>
-Thank you for choosing RP Clinic as your partner in your journey towards optimal orthopedic health. Together, let's pave the way towards a healthier, happier tomorrow.
-<br/>
-<br/>
-Sincerely,
-<br/>
-<br/>
-Dr. Sanjay Kumar<br/>
-MBBS,D-ORTHO,DNB-ORTHO<br/>
-RP Clinic
+        Dear Valued Patients,
+        <br />
+        <br />
+        Welcome to RP Clinic, where we prioritize your orthopedic health and well-being above all else. As a leading establishment in the field of orthopedics within the health domain, our mission is crystal clear: to provide unparalleled quality health services that cater to your unique needs and exceed your expectations.
+        <br />
+        <br />
+        At RP Clinic, we envision a future where every individual can enjoy a life free from the limitations of orthopedic conditions. Our commitment to excellence drives us to continuously innovate, educate, and deliver compassionate care to each and every patient who walks through our doors.
+        <br />
+        <br />
+        Thank you for choosing RP Clinic as your partner in your journey towards optimal orthopedic health. Together, let's pave the way towards a healthier, happier tomorrow.
+        <br />
+        <br />
+        Sincerely,
+        <br />
+        <br />
+        Dr. Sanjay Kumar<br />
+        MBBS,D-ORTHO,DNB-ORTHO<br />
+        RP Clinic
       </div>
     </div>
   )
