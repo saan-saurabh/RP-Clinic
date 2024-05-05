0da366cd01ac780e8ffd27b4847ef948e2b73d63
saan-saurabh <saurabhanshraj@gmail.com>
2024-05-05 20:45:07 +0530

    Added and Modified Navbar for small screen

--- rp-clinic/src/Components/Header/Navbar.js
+++ rp-clinic/src/Components/Header/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, } from "react-router-dom";
 import Modal from '../Modal/Modal';
 import Home from '../../Routes/Home';
@@ -7,28 +7,45 @@ import Contact from '../../Routes/Contact';
 import About from '../../Routes/About';
 import Logo from './Logo';
 import './Navbar.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar({ isOpen, onClose, onOpen }) {
+  const [sideBarMenuListState, setSideBarMenuListState] = useState(false);
+  const openSideBarMenuList = () => {
+    setSideBarMenuListState(!sideBarMenuListState);
+  }
   return (
     <Router>
       <div className='navbar-main-div'>
         <div>
           <Logo />
         </div>
-        <div className='navbar-make-appointment-button'>
-          <div className='navbar-right-pane'>
-            <Link to="/"></Link>
-            <Link to="/home" className='link-menu'><button className='list-menu'>HOME</button></Link>
-            <Link to="/services" className='link-menu'><button className='list-menu'>SERVICES</button></Link>
-            <Link to="/contact" className='link-menu'><button className='list-menu'>CONTACT</button></Link>
-            <Link to="/about" className='link-menu'><button className='list-menu'>ABOUT</button></Link>
-            <button className='list-button' onClick={onOpen}>APPOINTMENT</button>
-            <Modal isOpen={isOpen} onClose={onClose}/>
-          </div>
-        </div>
+        {((window.screen.width) > 992) ?
+          <div className='navbar-make-appointment-button'>
+            <div className='navbar-right-pane'>
+              <Link to="/"></Link>
+              <Link to="/home" className='link-menu'><button className='list-menu'>HOME</button></Link>
+              <Link to="/services" className='link-menu'><button className='list-menu'>SERVICES</button></Link>
+              <Link to="/contact" className='link-menu'><button className='list-menu'>CONTACT</button></Link>
+              <Link to="/about" className='link-menu'><button className='list-menu'>ABOUT</button></Link>
+              <button className='list-button' onClick={onOpen}>APPOINTMENT</button>
+              <Modal isOpen={isOpen} onClose={onClose} />
+            </div>
+          </div> : <MenuIcon fontSize='large' className='navbar-hamburger-icon' onClick={openSideBarMenuList} />}
       </div>
+      {sideBarMenuListState ?
+        <div className='sidebar-main-div'>
+          <div className='sidebar-right-pane'>
+            <Link to="/"></Link>
+            <Link to="/home" className='link-menu'><button className='list-menu' onClick={openSideBarMenuList}>HOME </button></Link>
+            <Link to="/services" className='link-menu'><button className='list-menu' onClick={openSideBarMenuList}>SERVICES</button></Link>
+            <Link to="/contact" className='link-menu'><button className='list-menu' onClick={openSideBarMenuList}>CONTACT</button></Link>
+            <Link to="/about" className='link-menu'><button className='list-menu' onClick={openSideBarMenuList}>ABOUT</button></Link>
+            <button className='list-menu' onClick={onOpen}>APPOINTMENT</button>
+            <Modal isOpen={isOpen} onClose={onClose} />
+          </div></div> : null}
       <Routes>
-      <Route
+        <Route
           exact
           path="/"
           element={<Home />}
@@ -38,17 +55,17 @@ function Navbar({ isOpen, onClose, onOpen }) {
           path="/home"
           element={<Home />}
         ></Route>
-                <Route
+        <Route
           exact
           path="/services"
           element={<Services />}
         ></Route>
-                        <Route
+        <Route
           exact
           path="/contact"
           element={<Contact />}
         ></Route>
-                        <Route
+        <Route
           exact
           path="/about"
           element={<About />}
